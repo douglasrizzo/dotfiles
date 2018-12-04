e79544b58460152c150cd1c32aee0712b1b64d08
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2018-12-04 17:43:07 -0200

    symbolic links are hard to make

--- install.sh
+++ install.sh
@@ -1,17 +1,21 @@
-ln -vsf .Xresources ~/.Xresources
-ln -vsf .profile ~/.profile
-ln -vsf .xprofile ~/.xprofile
-ln -vsf .zshrc ~/.zshrc
+# user-level files
+ln -vsf `pwd`/.zshrc ~/.zshrc
+ln -vsf `pwd`/.Xresources ~/.Xresources
+ln -vsf `pwd`/.profile ~/.profile
+ln -vsf `pwd`/.xprofile ~/.xprofile
+ln -vsf `pwd`/.profile ~/.zprofile
 
-ln -vsf .profile ~/.zprofile
 
-ln -vsf compton.conf ~/.config/compton.conf
-ln -vsf conky/ ~/.config/conky/
-ln -vsf dunst/ ~/.config/dunst/
-ln -vsf htop/ ~/.config/htop/
-ln -vsf i3-scrot.conf ~/.config/i3-scrot.conf
-ln -vsf i3/ ~/.config/i3/
-ln -vsf latexindent_config.yaml ~/.config/latexindent_config.yaml
-ln -vsf polybar/ ~/.config/polybar/
-ln -vsf rofi/ ~/.config/rofi/
-ln -vsf terminator/ ~/.config/terminator/
+# .config files
+ln -vsf `pwd`/compton.conf ~/.config/compton.conf
+ln -vsf `pwd`/latexindent_config.yaml ~/.config/latexindent_config.yaml
+ln -vsf `pwd`/i3-scrot.conf ~/.config/i3-scrot.conf
+
+# .config directories
+# these are not erased if they exist
+ln -vsf `pwd`/dunst/ ~/.config/dunst
+ln -vsf `pwd`/htop/ ~/.config/htop
+ln -vsf `pwd`/i3/ ~/.config/i3
+ln -vsf `pwd`/polybar/ ~/.config/polybar
+ln -vsf `pwd`/rofi/ ~/.config/rofi
+ln -vsf `pwd`/terminator/ ~/.config/terminator

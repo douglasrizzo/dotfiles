d06774ba34e57402a1fa1507e6b2e2bf69c49a21
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2018-12-04 17:10:07 -0200

    created an installation script that uses symbolic links

---
+++ install.sh
@@ -0,0 +1,17 @@
+ln -vsf .Xresources ~/.Xresources
+ln -vsf .profile ~/.profile
+ln -vsf .xprofile ~/.xprofile
+ln -vsf .zshrc ~/.zshrc
+
+ln -vsf .profile ~/.zprofile
+
+ln -vsf compton.conf ~/.config/compton.conf
+ln -vsf conky/ ~/.config/conky/
+ln -vsf dunst/ ~/.config/dunst/
+ln -vsf htop/ ~/.config/htop/
+ln -vsf i3-scrot.conf ~/.config/i3-scrot.conf
+ln -vsf i3/ ~/.config/i3/
+ln -vsf latexindent_config.yaml ~/.config/latexindent_config.yaml
+ln -vsf polybar/ ~/.config/polybar/
+ln -vsf rofi/ ~/.config/rofi/
+ln -vsf terminator/ ~/.config/terminator/

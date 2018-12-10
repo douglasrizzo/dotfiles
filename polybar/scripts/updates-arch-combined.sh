6b98f54fe62f3343b3c16a78bcf5985f9f855cce
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2018-12-10 14:42:53 -0200

    polybar: arch-updates left-click starts octopi

--- polybar/scripts/updates-arch-combined.sh
+++ polybar/scripts/updates-arch-combined.sh
@@ -1,19 +1,42 @@
 #!/bin/sh
 
-if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
-    updates_arch=0
-fi
+start_pm() {
+    if type pamac | grep 'pamac is'; then
+        notify-send 'Starting pamac'
+        pamac
+    elif type octopi | grep 'octopi is'; then
+        notify-send 'Starting octopi'
+        octopi
+    else
+        notify-send 'No package manager found (pamac or octopi)'
+    fi
+}
 
-if ! updates_aur=$(yay -Qum | wc -l); then
-# if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
-# if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
-    updates_aur=0
-fi
+main(){
+    if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+        updates_arch=0
+    fi
 
-updates=$(("$updates_arch" + "$updates_aur"))
+    if ! updates_aur=$(yay -Qum | wc -l); then
+    # if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
+    # if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
+        updates_aur=0
+    fi
 
-if [ "$updates" -gt 0 ]; then
-    echo " $updates"
-else
-    echo ""
-fi
+    updates=$(("$updates_arch" + "$updates_aur"))
+
+    if [ "$updates" -gt 0 ]; then
+        echo " $updates"
+    else
+        echo ""
+    fi
+}
+
+case "$1" in
+    --gui)
+        start_pm
+        ;;
+    *)
+        main
+        ;;
+esac

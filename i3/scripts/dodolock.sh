8a79af386f659689bcce681792abf94eaa6af95e
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2019-04-08 18:20:23 -0300

    lock script starts xcreensaver daemon if it`s not started

--- i3/scripts/dodolock.sh
+++ i3/scripts/dodolock.sh
@@ -2,10 +2,21 @@
 
 # Screen lock
 # this one uses i3lock and displays images from a directory
-# i3lock -c 0F121B -e -f -i $(find $1 -maxdepth 2 -name '*.png' -type f | shuf | head -n 1) -p win 
+# i3lock -c 0F121B -e -f -i $(find $1 -maxdepth 2 -name '*.png' -type f | shuf | head -n 1) -p win
 
 # this one uses xscreensaver
-xscreensaver-command -lock
+if ! pidof -s xscreensaver >/dev/null; then
+    notify-send 'starting xscreensaver daemon, wait a few seconds...'
+    xscreensaver &
+
+    until pids=$(pidof xscreensaver); do
+        sleep 1
+    done
+
+    notify-send 'daemon started, try locking now'
+else
+    xscreensaver-command -lock
+fi
 
 # this one uses blurlock, a program that comes with manjaro-3
 # blurlock

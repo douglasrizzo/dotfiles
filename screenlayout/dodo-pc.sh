01cde128c80427c6416cb046d845caae6c4cda1c
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2019-08-13 00:56:07 -0300

    xrandr: changed monitor layout for dodo-pc

--- screenlayout/dodo-pc.sh
+++ screenlayout/dodo-pc.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-0 --primary --mode 1920x1080 --rotate normal
+xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 1440x0 --rotate normal --output DP-0 --mode 1440x900 --pos 0x90 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off

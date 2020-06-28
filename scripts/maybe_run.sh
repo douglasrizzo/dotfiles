eef7175af099db3785cf488e7a05f179d7a6e291
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2020-06-28 07:22:39 -0300

    expand maybe_run for the case when command and executable are different

--- scripts/maybe_run.sh
+++ scripts/maybe_run.sh
@@ -1,9 +1,9 @@
-pidof $1
+pidof ${2:-$1}
 r=$?
 
 case $r in
 0)
-    notify-send "$1 already running!"
+    notify-send "${2:-$1} already running!"
     ;;
 1)
     notify-send "launching $1..." && $1 || "command $1 failed"

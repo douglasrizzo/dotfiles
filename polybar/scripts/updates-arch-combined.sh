af67b26e6b0636a551dd336bab3c77a19dd1e586
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2020-08-14 22:07:20 -0300

    too tired to describe

--- polybar/scripts/updates-arch-combined.sh
+++ polybar/scripts/updates-arch-combined.sh
@@ -1,42 +1,38 @@
 #!/bin/sh
 
 start_pm() {
-    if type pamac | grep 'pamac is'; then
-        notify-send 'Starting pamac'
-        pamac
-    elif type octopi | grep 'octopi is'; then
+    if type octopi | grep 'octopi is'; then
         notify-send 'Starting octopi'
         octopi
+    elif type pamac-manager | grep 'pamac-manager is'; then
+        notify-send 'Starting pamac'
+        pamac-manager
     else
-        notify-send 'No package manager found (pamac or octopi)'
+        notify-send 'No package manager found (octopi or pamac)'
     fi
 }
 
-main(){
-    if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+main() {
+    if ! updates_arch=$(checkupdates 2>/dev/null | wc -l); then
         updates_arch=0
     fi
 
-    if ! updates_aur=$(yay -Qum | wc -l); then
-    # if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
-    # if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
+    if ! updates_aur=$(yay -Qum 2>/dev/null | wc -l); then
         updates_aur=0
     fi
 
     updates=$(("$updates_arch" + "$updates_aur"))
 
-    if [ "$updates" -gt 0 ]; then
-        echo " $updates"
-    else
-        echo ""
+    if [ $updates -gt 0 ]; then
+        echo " $updates_arch/$updates_aur"
     fi
 }
 
 case "$1" in
-    --gui)
-        start_pm
-        ;;
-    *)
-        main
-        ;;
+--gui)
+    start_pm
+    ;;
+*)
+    main
+    ;;
 esac

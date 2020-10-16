497d8896d24327dbf452f152cf593f76410b9a52
Douglas <douglasrizzom@gmail.com>
2020-10-15 21:28:34 -0300

    add apple keyboard to kb-layout.sh

--- scripts/kb-layout.sh
+++ scripts/kb-layout.sh
@@ -1,8 +1,11 @@
-ANSWER=$(zenity --list --title="Keyboard Layout" --text "Pick keyboard layout below" --radiolist --column "" --column "Layout" TRUE "QWERTY ABNT-2" FALSE "Dvorak right-handed" FALSE "Dvorak")
+ANSWER=$(zenity --list --title="Keyboard Layout" --text "Pick keyboard layout below" --radiolist --column "" --column "Layout" TRUE "QWERTY ABNT-2" FALSE "Apple US Intl" FALSE "Dvorak right-handed" FALSE "Dvorak")
 
 case $ANSWER in
 "QWERTY ABNT-2")
     notify-send $ANSWER && setxkbmap -layout br -variant abnt2
+    ;;
+"Apple US Intl")
+    notify-send "$ANSWER" && setxkbmap -model apple -layout us -variant intl
     ;;
 "Dvorak right-handed")
     notify-send $ANSWER && setxkbmap -layout br -variant dvorak

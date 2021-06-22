a2cf58044149bd6010e28c88946cd438559f2ef5
Douglas <douglasrizzom@gmail.com>
2021-06-22 02:11:17 -0300

    change change_sinks.sh scriptto work on PC and laptop (unstable)

--- scripts/change_sinks.sh
+++ scripts/change_sinks.sh
@@ -1,1 +1,7 @@
-pacmd list-sinks | grep -e '  index:' | awk -F'index:' '{for (i=2; i<=NF; i++) printf("%s", $i)}' | xargs pacmd set-default-sink
+if pacmd list-sinks | grep -e '  index:'; then
+  pacmd list-sinks | grep -e '  index:' | awk -F'index:' '{for (i=2; i<=NF; i++) printf("%s", $i)}' | xargs pacmd set-default-sink
+elif pacmd list-sinks | grep -e 'active port: <analog-output-speaker>'; then
+  pacmd set-sink-port 0 analog-output-headphones
+else
+  pacmd set-sink-port 0 analog-output-speaker
+fi

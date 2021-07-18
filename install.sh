35fa1e9dfb76f4296337c0069ed55ada37e1dc56
Douglas <douglasrizzom@gmail.com>
2021-07-18 15:48:05 -0300

    put all xorg conf files into a mock xorg.conf.d directory

--- install.sh
+++ install.sh
@@ -39,8 +39,7 @@ link_stuff() {
   ln -vsf $(pwd)/latexindent/config.yaml ~/.config/latexindent.yaml
   ln -vsf $(pwd)/latexindent/config_location.yaml ~/.indentconfig.yaml
   ln -vsf $(pwd)/alacritty.yml ~/.config/alacritty.yml
-  sudo ln -vsf $(pwd)/10-libinput.conf /etc/X11/xorg.conf.d/10-libinput.conf
-  sudo ln -vsf $(pwd)/10-monitor.conf /etc/X11/xorg.conf.d/10-monitor.conf
+  ln -vsf $(pwd)/xorg.conf.d/* /etc/X11/xorg.conf.d/
 
   ln -vsf $(pwd)/pywalfox_update.sh ~/.config/pywalfox_update.sh
 

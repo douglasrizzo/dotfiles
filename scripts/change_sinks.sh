510e92bad6ffe8975f7f9b197bccaedcc1b76a74
Douglas <douglasrizzom@gmail.com>
2022-10-04 17:16:27 -0300

    fix change_sinks script

--- scripts/change_sinks.sh
+++ scripts/change_sinks.sh
@@ -1,8 +1,11 @@
 # A script that allows a the user to select a different default audio sink using dmenu
 
 # If more than one non-default sink available, all sinks are listed for choosing
-if pactl list sinks | grep -e 'State: RUNNING'; then
-  pactl list sinks | grep -e 'Name:' | sed -r 's/.*Name: (.*)/\1/' | dmenu -l 5 | xargs pactl set-default-sink
+if (( $(pactl list sinks | grep -e 'State:' | wc -l) > 1 )); then
+  VAL=$(pactl list sinks | grep -e 'Name:' | sed -r 's/.*Name: (.*)/\1/' | sort | dmenu -l 5)
+  if [ "$VAL" != "" ]; then
+    pactl set-default-sink $VAL
+  fi
 # else, swap the port of the current sink between headphone and speaker
 elif pactl list sinks | grep -e 'Active port: analog-output-speaker'; then
   pactl set-sink-port 0 analog-output-headphones

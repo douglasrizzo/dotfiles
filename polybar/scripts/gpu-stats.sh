91dc0cd702b15a6da22578f6fcb108be498ec26d
Douglas <douglasrizzom@gmail.com>
2023-03-16 03:04:57 -0300

    [scripts] smaller gpu-stats out

--- polybar/scripts/gpu-stats.sh
+++ polybar/scripts/gpu-stats.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-nvidia-smi --query-gpu memory.used,memory.total,temperature.gpu,utilization.gpu,pstate --format=csv,noheader,nounits -i 0 | awk '{
+nvidia-smi --query-gpu memory.used,memory.total,temperature.gpu,utilization.gpu --format=csv,noheader,nounits -i 0 | awk '{
     split($0,a,", ");
-    printf "%s %.d/%.dMiB (%.1f%%), %.d C, %.d%%", a[5],a[1],a[2],a[1]/a[2]*100,a[3],a[4]
+    printf "%.dMiB (%d%%), %dC, %.d%%", a[1],a[1]/a[2]*100,a[3],a[4]
     }'

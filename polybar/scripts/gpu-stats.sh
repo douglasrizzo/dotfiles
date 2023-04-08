48032ae4bfabe5af13e7890e3e410877c7bfc781
Douglas <douglasrizzom@gmail.com>
2023-04-08 01:14:17 -0300

    [gpu-stats] add default size

--- polybar/scripts/gpu-stats.sh
+++ polybar/scripts/gpu-stats.sh
@@ -2,5 +2,5 @@
 
 nvidia-smi --query-gpu memory.used,memory.total,temperature.gpu,utilization.gpu --format=csv,noheader,nounits -i 0 | awk '{
     split($0,a,", ");
-    printf "%.dMiB (%d%%), %dC, %.d%%", a[1],a[1]/a[2]*100,a[3],a[4]
+    printf "%.dMiB (%d%%),%dC,%.2d%%", a[1],a[1]/a[2]*100,a[3],a[4]
     }'

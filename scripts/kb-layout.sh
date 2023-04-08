df63d55401dc1c1a2a3727a57b0a9cf2283deebd
Douglas <douglasrizzom@gmail.com>
2023-04-08 01:09:09 -0300

    [sxhkd] stop using kb-layout.sh

--- scripts/kb-layout.sh
+++
@@ -1,9 +0,0 @@
-if setxkbmap -query | grep 'layout:     us'; then
-    setxkbmap -model p104 -layout br -variant abnt2 && \
-    dunstify -a setxkbmap 'Changing keyboard to ABNT-2' || \
-    dunstify -a setxkbmap 'There was a problem changing keyboard mappings!'
-else
-    setxkbmap -model p104 -layout us -variant intl && \
-    dunstify -a setxkbmap 'Changing keyboard to US International' || \
-    dunstify -a setxkbmap 'There was a problem changing keyboard mappings!'
-fi

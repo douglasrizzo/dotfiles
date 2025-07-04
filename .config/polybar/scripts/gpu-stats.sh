c30aca4de0e5dd62d3ebb27dc4db32228f274562
Douglas De Rizzo Meneghetti <douglasrizzom@gmail.com>
2025-07-04 13:39:17 -0300

    print error on polybar if stats can't be queried

--- .config/polybar/scripts/gpu-stats.sh
+++ .config/polybar/scripts/gpu-stats.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-nvidia-smi --query-gpu memory.used,memory.total,temperature.gpu,utilization.gpu --format=csv,noheader,nounits -i 0 | awk '{
-    split($0,a,", ");
-    printf "%.dMiB (%d%%),%dC,%.2d%%", a[1],a[1]/a[2]*100,a[3],a[4]
+# Nerd Font Icons
+ICON_VRAM="󰍛" # microchip
+ICON_TEMP="󰔄" # thermometer
+ICON_UTIL="󱄄" # speedometer
+
+if output=$(nvidia-smi --query-gpu memory.used,memory.total,temperature.gpu,utilization.gpu --format=csv,noheader,nounits -i 0 2>/dev/null); then
+    echo "$output" | awk -v vram="$ICON_VRAM" -v temp="$ICON_TEMP" -v util="$ICON_UTIL" '{
+        split($0,a,", ");
+        printf "%s %4dMiB (%3d%%) | %d%s | %s %3d%%", vram, a[1], a[1]/a[2]*100, a[3], temp, util, a[4]
     }'
+else
+    echo "GPU stats not available"
+fi
